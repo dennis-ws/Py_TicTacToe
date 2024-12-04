7bd67ac47320ecd4b29b78f9dddbf2d5fe828547
Dennis <dennissung6105@gmail.com>
2024-12-04 18:34:44 +1000

    Removed Player class and added winning triad

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,8 @@
-# Player (to remember board state)
-class Player():
-    def __init__(self):
-        pass
+winning_triad = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],    # Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],    # Columns
+    [0, 4, 8], [2, 4, 6]                # Diagonals
+]
 
 # Board (serves as GUI mostly)
 class Board():
@@ -11,10 +12,19 @@ class Board():
 # Controller
 class Game():
     def __init__(self):
-        pass
+        self.board_state = [None] * 9
+        self.board = Board()
+        self.winner = None
 
-    def play():
-        pass
+    def play(self):
+        while not self.has_winner():
+            # We do something
+            break
+
+    def has_winner(self) -> bool:
+        if self.winner:
+            return True
+        return False
 
 if __name__ == "__main__":
     game = Game()
