ab290864f3c3b5efa8899da96feb6d72e7c14553
Dennis <dennissung6105@gmail.com>
2024-12-04 18:13:47 +1000

    First Commit and initialised starting classes

---
+++ tictactoe.py
@@ -0,0 +1,21 @@
+# Player (to remember board state)
+class Player():
+    def __init__(self):
+        pass
+
+# Board (serves as GUI mostly)
+class Board():
+    def __init__(self):
+        pass
+
+# Controller
+class Game():
+    def __init__(self):
+        pass
+
+    def play():
+        pass
+
+if __name__ == "__main__":
+    game = Game()
+    game.play()
